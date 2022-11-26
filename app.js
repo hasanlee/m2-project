635bb9326023db37b4f5efd331745d35bcbf16e0
Hasanli Hasan <hasan@hasanli.info>
2022-11-26 10:52:54 +0400

    added drag and drop function

--- app.js
+++ app.js
@@ -49,7 +49,15 @@ function getTodos() {
   todos.forEach((todo, index) => {
     let div = document.createElement("div");
     div.classList.add("list-item");
+    div.classList.add("draggable");
+    div.setAttribute("draggable", true);
     div.setAttribute("data-order", index);
+    div.addEventListener("dragstart", dragStart, false);
+    div.addEventListener("dragenter", dragEnter, false);
+    div.addEventListener("dragover", dragOver, false);
+    div.addEventListener("dragleave", dragLeave, false);
+    div.addEventListener("drop", dragDrop, false);
+    div.addEventListener("dragend", dragEnd, false);
     div.innerHTML = `<p>${todo.text}</p>
     <svg onclick="removeTodo('${todo.id}',this)" width="20" height="20" class="icon deleteBtn" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
@@ -93,4 +101,38 @@ function uuidv4() {
       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
     ).toString(16)
   );
+}
+// Draggable
+var remove = document.querySelector(".draggable");
+function dragStart(e) {
+  this.style.opacity = "0.4";
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/html", this.innerHTML);
+}
+function dragEnter(e) {
+  this.classList.add("over");
+}
+function dragLeave(e) {
+  e.stopPropagation();
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = "move";
+  return false;
+}
+function dragDrop(e) {
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData("text/html");
+  }
+  return false;
+}
+function dragEnd(e) {
+  var listItens = document.querySelectorAll(".draggable");
+  [].forEach.call(listItens, function (item) {
+    item.classList.remove("over");
+  });
+  this.style.opacity = "1";
 }
