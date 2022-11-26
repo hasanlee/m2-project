edb9ee67c2905073d9a181f3eddb0d74ff143061
Hasanli Hasan <hasan@hasanli.info>
2022-11-26 10:08:51 +0400

    some bug fixes and improvements

--- app.js
+++ app.js
@@ -5,51 +5,57 @@ const textInput = document.querySelector("#text");
 const list = document.querySelector(".list");
 const addTodoBtn = document.querySelector("#addTodoBtn");
 const sortBtn = document.querySelector("#sortBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
 
+document.addEventListener("load", getTodos());
 addTodoBtn.addEventListener("click", (e) => {
   let id = uuidv4();
   let text = textInput.value;
-  let status = false;
-  addTodo(id, text, status);
+  addTodo(id, text);
   textInput.value = "";
   getTodos();
 });
 textInput.addEventListener("keyup", (e) => {
-  if (
-    e.code == "Enter" &&
-    textInput.value != undefined &&
-    textInput.value != ""
-  ) {
+  if (e.code == "Enter") {
     let id = uuidv4();
     let text = textInput.value;
-    let status = false;
-    addTodo(id, text, status);
+    addTodo(id, text);
     textInput.value = "";
     getTodos();
   }
 });
 sortBtn.addEventListener("click", sortList);
-
-function clearInput() {
+deleteBtn.addEventListener("click", () => {
   textInput.value = "";
+});
+function addTodo(id, text) {
+  if (text != undefined && text != "") {
+    todos.push({
+      id: id,
+      text: text,
+    });
+  }
 }
-
-function addTodo(id, text, status) {
-  todos.push({
-    id: id,
-    text: text,
-    status: status,
-  });
-  let div = document.createElement("div");
-  div.classList.add("list-item");
-  div.innerHTML = `<p>${text}</p><img class="icon" id="deleteBtn" src="./icons/delete.png" onclick="removeTodo('${id}',this)" />`;
-  list.append(div);
-}
-
 function getTodos() {
-  console.log(todos);
+  todos.length <= 0
+    ? (list.style.border = "0px")
+    : (list.style.border = "1px solid #c4c4c4");
+  list.replaceChildren();
+  todos.forEach((todo, index) => {
+    let div = document.createElement("div");
+    div.classList.add("list-item");
+    div.setAttribute("data-order", index);
+    div.addEventListener("drag", drag);
+    div.addEventListener("drop", drop);
+    div.innerHTML = `<p>${todo.text}</p>
+    <svg onclick="removeTodo('${todo.id}',this)" width="20" height="20" class="icon deleteBtn" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
+    <path d="M6 6L14 14" stroke="#C4C4C4"/>
+    <path d="M6 14L14 6" stroke="#C4C4C4"/>
+    </svg>`;
+    list.append(div);
+  });
 }
-
 function removeTodo(id, el) {
   let index = todos.findIndex((todo) => todo.id == id);
   if (index > -1) {
@@ -58,7 +64,6 @@ function removeTodo(id, el) {
   el.parentNode.remove();
   getTodos();
 }
-
 function sortList() {
   if (sortAsc) {
     todos.sort((a, b) => {
@@ -75,7 +80,6 @@ function sortList() {
   }
   getTodos();
 }
-
 function uuidv4() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
     (
